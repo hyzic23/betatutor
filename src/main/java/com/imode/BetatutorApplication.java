10750b14525f4f0cdc596699f9bd6e718e1006ae
hyzic <isaac.oghenevwapo@gmail.com>
2024-11-28 01:29:57 -0500

    First Commit

---
+++ src/main/java/com/imode/BetatutorApplication.java
@@ -0,0 +1,13 @@
+package com.imode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetatutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetatutorApplication.class, args);
+	}
+
+}
